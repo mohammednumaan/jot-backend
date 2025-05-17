879d0394d041f96d54850a7619c8aca74cc3ac95
Mohammed Numaan <138296610+mohammednumaan@users.noreply.github.com>
2025-05-17 14:32:56 +0530

    chore: configure eslint.

---
+++ eslint.config.mjs
@@ -0,0 +1,16 @@
+import js from "@eslint/js";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+import { defineConfig } from "eslint/config";
+
+export default defineConfig([
+  {
+    files: ["**/*.{js,mjs,cjs,ts}"],
+    languageOptions: { globals: globals.node },
+  },
+  { ignores: [".node_modules/*", "./generated/*", "./prisma/*"] },
+
+  js.configs.recommended,
+  tseslint.configs.recommended,
+  
+]);
