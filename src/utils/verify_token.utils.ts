a7d7bf1fc78af60e6248015f69ac0a18c4aea41a
Mohammed Numaan <138296610+mohammednumaan@users.noreply.github.com>
2025-05-19 22:46:13 +0530

    feat: transform jsonwebtoken error's to custom errors.

--- src/utils/verify_token.utils.ts
+++ src/utils/verify_token.utils.ts
@@ -1,10 +1,11 @@
 import jwt from "jsonwebtoken";
 import { Request } from "express";
 import { envData } from "../env";
+import { UnauthorizedError } from "../errors/api/error";
 export default function verifyToken(req: Request, token: string) {
   jwt.verify(token, envData.ACCESS_TOKEN_SECRET, (err, user) => {
     if (err) {
-      throw err;
+      throw new UnauthorizedError(err.message);
     }
     req.user = user;
   });
